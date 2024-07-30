e0477542392f3505a3bdc6d57d6698c8b5f5b8bf
Alistair <AlistairJR302@Gmail.com>
2024-07-30 22:42:07 +0100

    Tidied up code, moved into functions and added comments

--- decoding_secret_message.py
+++ decoding_secret_message.py
@@ -5,10 +5,8 @@ def print_grid(array):
     for row in array:
         print(*row)
 
-def main():
+def main(url):
     # Read the table from the given url
-    url = r'https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub'
-    # url = r'https://docs.google.com/document/d/e/2PACX-1vSHesOf9hv2sPOntssYrEdubmMQm8lwjfwv6NPjjmIRYs_FOYXtqrYgjh85jBUebK9swPXh_a5TJ5Kl/pub'
     table = pd.read_html(url, encoding='utf-8')[0]
 
     # Separate the coordinates and characters into separate arrays by column
@@ -20,13 +18,18 @@ def main():
     xcoordinates = [int(string_coord) for string_coord in xcoordinates]
     ycoordinates = [int(string_coord) for string_coord in ycoordinates]
 
-    grid = [['*' for i in range(max(xcoordinates)+1)] for j in range(max(ycoordinates)+1)]
+    # Initialise an empty grid to display the message in 
+    grid = [['*' for _ in range(max(xcoordinates)+1)] for _ in range(max(ycoordinates)+1)]
 
+    # Iterate through x coordinates and place the corresponding character into the grid
     for x_idx, x_coord in enumerate(xcoordinates):
+        # Corrects the y coordinate so y=0 is at the bottom
         grid[max(ycoordinates)-ycoordinates[x_idx]][x_coord] = characters[x_idx]
 
     print_grid(grid)
 
 
 if __name__ == "__main__":
-    main()
+    # url = r'https://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub'
+    url = r'https://docs.google.com/document/d/e/2PACX-1vSHesOf9hv2sPOntssYrEdubmMQm8lwjfwv6NPjjmIRYs_FOYXtqrYgjh85jBUebK9swPXh_a5TJ5Kl/pub'
+    main(url)
