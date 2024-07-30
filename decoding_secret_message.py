fa3972c5a22d7cb2c20cec787c45c61b95a5bf0c
Alistair <AlistairJR302@Gmail.com>
2024-07-30 21:51:04 +0100

    Retrieved and printed the table from the url

---
+++ decoding_secret_message.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from array import *
+
+url = r'http://docs.google.com/document/d/e/2PACX-1vRMx5YQlZNa3ra8dYYxmv-QIQ3YJe8tbI3kqcuC7lQiZm-CSEznKfN_HYNSpoXcZIV3Y_O3YoUB1ecq/pub'
+table = pd.read_html(url, encoding='utf-8')[0]
+print(table)
